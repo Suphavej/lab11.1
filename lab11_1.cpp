915c33d4ac0f6beaf00041f5f223049d9811f9f5
Suphavej <fatkimmmm@gmail.com>
2025-12-30 14:33:20 +0700

    sedf

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    
+    srand(time(0));
+    int randomIndex = rand() % 9;
+
+    cout << endl;
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}
